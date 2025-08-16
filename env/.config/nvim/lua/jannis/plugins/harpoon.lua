96035336e9374a0826d152bec0e7e8e447f05fb7
Jannis <jannis2401p@gmail.com>
2025-08-16 18:57:39 +0200

    automated dev commit

--- env/.config/nvim/lua/jannis/plugins/harpoon.lua
+++ env/.config/nvim/lua/jannis/plugins/harpoon.lua
@@ -1,24 +1,23 @@
+
+-- lua/plugins/harpoon.lua
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    config = function()
-        local harpoon = require("harpoon")
-        local ui = require("harpoon.ui")
+  "ThePrimeagen/harpoon",
+  keys = {
+    { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Add file to Harpoon" },
+    { "<leader>h", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon menu" },
+    { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Go to Harpoon file 1" },
+    { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Go to Harpoon file 2" },
+    { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Go to Harpoon file 3" },
+    { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Go to Harpoon file 4" },
+  },
+  config = function()
+    local harpoon = require("harpoon")
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
 
-        harpoon.setup({
-            menu = {
-                width = vim.api.nvim_win_get_width(0) - 4,
-            }
-        })
-
-        -- Dateien hinzufügen direkt über harpoon.add_file
-        vim.keymap.set("n", "<leader>a", function() harpoon.add_file() end)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-        vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end)
-    end
+    -- Optional: weitere Keymaps
+    vim.keymap.set("n", "<leader>m", mark.add_file, { desc = "Add file to Harpoon" })
+    vim.keymap.set("n", "<leader>n", ui.toggle_quick_menu, { desc = "Toggle Harpoon menu" })
+  end
 }
 
