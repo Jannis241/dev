52adb32ffe795ba5cd7383041bf5788cdb087489
Jannis <jannis2401p@gmail.com>
2025-08-18 16:40:08 +0200

    automated dev commit

--- env/.config/nvim/lua/jannis/plugins/telescope.lua
+++ env/.config/nvim/lua/jannis/plugins/telescope.lua
@@ -1,63 +1,66 @@
 return {
-	"nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
 
-	tag = "0.1.8",
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	defaults = {
-		prompt_prefix = "   ",
-		selection_caret = " ",
-		entry_prefix = " ",
-		sorting_strategy = "ascending",
-		layout_config = {
-			horizontal = {
-				prompt_position = "top",
-				preview_width = 0.55,
-			},
-			width = 0.87,
-			height = 0.80,
-		},
-		mappings = {
-			n = { ["q"] = require("telescope.actions").close },
-		},
-	},
+    telescope.setup({
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = " ",
+        entry_prefix = " ",
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          width = 0.87,
+          height = 0.80,
+        },
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["q"] = actions.close,
+          },
+        },
+      },
+      pickers = {
+        find_files = { hidden = true },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- Fuzzy Match
+          override_generic_sorter = true,  -- ersetzt sorter
+          override_file_sorter = true,
+          case_mode = "smart_case",        -- smart case
+        },
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}) -- nicer dropdown
+        }
+      },
+    })
 
-	extensions_list = { "themes", "terms" },
-	extensions = {},
+    -- Extensions laden
+    telescope.load_extension("fzf")
+    telescope.load_extension("ui-select")
 
-	config = function()
-		local telescope = require("telescope")
-		local builtin = require("telescope.builtin")
-
-		telescope.setup({
-			pickers = {
-				find_files = {
-					hidden = true,
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>pws", builtin.current_buffer_fuzzy_find, {})
-		vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
-		vim.keymap.set("n", "<leader>pg", function()
-			local search = vim.fn.input("Grep > ")
-			local cmd = string.format("silent! grep %s **/*", vim.fn.shellescape(search))
-			vim.cmd(cmd)
-			vim.cmd("copen")
-		end)
-
-		vim.keymap.set("n", "<leader>pgs", function()
-			local search = vim.fn.input("Grep (current buffer) > ")
-			local current_file = vim.fn.expand("%:p") -- Vollständiger Pfad der aktuellen Datei
-			local cmd =
-				string.format("silent! grep %s %s", vim.fn.shellescape(search), vim.fn.shellescape(current_file))
-			vim.cmd(cmd)
-			vim.cmd("copen")
-		end)
-	end,
+    -- Keymaps
+    vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>pws", builtin.current_buffer_fuzzy_find, { desc = "Buffer fuzzy find" })
+    vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+  end,
 }
+
