c0e7323c3d6c48f4fb05eb98ce3a19851a0a8dcd
Jannis <jannis2401p@gmail.com>
2025-08-18 22:31:04 +0200

    automated dev commit

--- env/.config/nvim/lua/jannis/plugins/noice.lua
+++ env/.config/nvim/lua/jannis/plugins/noice.lua
@@ -1,28 +1,28 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-		"hrsh7th/nvim-cmp",
-	},
-	config = function()
-		require("noice").setup({
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
-			},
-			signature = {
-				enabled = false,
-			},
-
-			messages = {
-				enable = false,
-			},
-		})
-	end,
+	-- "folke/noice.nvim",
+	-- event = "VeryLazy",
+	-- opts = {},
+	-- dependencies = {
+	-- 	"MunifTanjim/nui.nvim",
+	-- 	"rcarriga/nvim-notify",
+	-- 	"hrsh7th/nvim-cmp",
+	-- },
+	-- config = function()
+	-- 	require("noice").setup({
+	-- 		lsp = {
+	-- 			override = {
+	-- 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+	-- 				["vim.lsp.util.stylize_markdown"] = true,
+	-- 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+	-- 			},
+	-- 		},
+	-- 		signature = {
+	-- 			enabled = false,
+	-- 		},
+	--
+	-- 		messages = {
+	-- 			enable = false,
+	-- 		},
+	-- 	})
+	-- end,
 }
--- env/.config/nvim/lua/jannis/plugins/themes.lua
+++ env/.config/nvim/lua/jannis/plugins/themes.lua
@@ -16,6 +16,22 @@ end
 
 return {
 	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("github-theme").setup({
+                options = {
+                    transparent = true,
+                }
+			})
+            -- namen: https://github.com/projekt0n/github-nvim-theme
+
+            apply_colorscheme("github_dark_default")
+		end,
+	},
+	{
 		"Shatur/neovim-ayu",
 		config = function()
 			require("ayu").setup({
@@ -35,6 +51,14 @@ return {
 				},
 			})
 			-- apply_colorscheme("ayu")
+		end,
+	},
+
+	{
+		"embark-theme/vim",
+		as = "embark",
+		config = function()
+			-- apply_colorscheme("embark")
 		end,
 	},
 
@@ -112,7 +136,7 @@ return {
 					transparency = true,
 				},
 			})
-			apply_colorscheme("rose-pine")
+			-- apply_colorscheme("rose-pine")
 		end,
 	},
 }
