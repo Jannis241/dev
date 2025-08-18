cd6ffe3c1621e7487d41cc5933675a283a9044ce
Jannis <jannis2401p@gmail.com>
2025-08-18 21:49:16 +0200

    automated dev commit

--- env/.config/nvim/lua/jannis/plugins/noice.lua
+++ env/.config/nvim/lua/jannis/plugins/noice.lua
@@ -5,22 +5,20 @@ return {
 	-- dependencies = {
 	-- 	"MunifTanjim/nui.nvim",
 	-- 	"rcarriga/nvim-notify",
-	--        "hrsh7th/nvim-cmp",
-	--
+	-- 	"hrsh7th/nvim-cmp",
 	-- },
 	-- config = function()
 	-- 	require("noice").setup({
 	-- 		lsp = {
-	-- 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 	-- 			override = {
 	-- 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 	-- 				["vim.lsp.util.stylize_markdown"] = true,
 	-- 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 	-- 			},
 	-- 		},
-	--            messages = {
-	--                enable = false,
-	--            },
+	-- 		messages = {
+	-- 			enable = false,
+	-- 		},
 	-- 		-- you can enable a preset for easier configuration
 	-- 		presets = {
 	-- 			bottom_search = true, -- use a classic bottom cmdline for search
