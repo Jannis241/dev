466665e84bbc7a093086031a426a47ce929aaf01
Jannis <jannis2401p@gmail.com>
2026-05-30 18:19:14 +0200

    chore(dev): automated snapshot 2026-05-30

--- env/.config/nvim/lua/jannis/plugins/neotree.lua
+++ env/.config/nvim/lua/jannis/plugins/neotree.lua
@@ -93,7 +93,7 @@ return {
 					leave_dirs_open = false,
 				},
 				group_empty_dirs = false,
-				hijack_netrw_behavior = "disabled",
+				hijack_netrw_behavior = "open_current",
 				use_libuv_file_watcher = false,
 			},
 		},
