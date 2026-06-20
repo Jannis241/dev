59ca3ff7512e683be8f93f47f636d3bee387e048
Jannis <jannis2401p@gmail.com>
2026-06-20 11:46:53 +0200

    chore(dev): automated snapshot 2026-06-20

--- env/.config/nvim/lua/plugins/treesitter.lua
+++ env/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,10 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
 		lazy = false,
+
 		config = function()
 			local parsers = {
 				"lua",
@@ -12,38 +14,17 @@ return {
 				"json",
 				"gitignore",
 				"go",
-                "bash",
-                "cpp",
-                "java",
-                "python",
-                "c",
-                "rust",
+				"bash",
+				"cpp",
+				"java",
+				"python",
+				"c",
+				"rust",
+				"markdown",
+				"markdown_inline",
 			}
 
-			local group = vim.api.nvim_create_augroup("TreesitterGroup", { clear = true })
-
-            -- Nach dem Start von nvim alle fehlenden parser installieren
-			vim.api.nvim_create_autocmd("User", {
-				group = group,
-				pattern = "Lazy",
-				once = true,
-				callback = function()
-					require("nvim-treesitter").install(parsers)
-				end,
-			})
-
-            -- Wenn ein buffer geöffnet wird, treesitter starten
-			vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-				group = group,
-				callback = function()
-					if vim.bo.buftype ~= "" then
-						return
-					end
-
-					pcall(vim.treesitter.start, 0)
-				end,
-			})
-
+			require("nvim-treesitter").install(parsers)
 		end,
 	},
 }
